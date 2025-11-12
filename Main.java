5dd07189c9385f28e0d81e27540a3db0833e8d81
Ghala1034 <ghala_2004.z@hotmail.com>
2025-11-12 09:41:56 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String coffee="Arabi";
+	    if(coffee.equals("Arabi")){
+		System.out.println("l have chosen Arabi coffee");
+	    } else if (coffee.equals("v60")){
+	        System.out.println("l have chosen v60 coffee");
+	    } else if (coffee.equals("cappuccino")){
+	        System.out.println("l have chosen cappuccino coffee");
+	    } else{
+	        System.out.println("l don’t know what coffee you chose");
+	    }
+	    }
+	    }
